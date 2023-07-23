ad4136f35ebbfe17ed15a93a79ac7dd87af5b13c
alex <alexsemen666653@gmail.com>
2023-07-23 10:20:20 +0300

    Выполнено ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,83 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task1();
+        determineYear(2020);
+        determineYear(2012);
+        determineYear(2019);
+        task2();
+        defineVersionApp(0,2022);
+        defineVersionApp(1, 2018);
+        defineVersionApp(0, 2023);
+        task3();
+        printNumberDaysDelivery(95);
+        printNumberDaysDelivery(120);
+        printNumberDaysDelivery(8);
+    }
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static void task1() {
+        System.out.println("Задача 1");
+    }
+    public static void determineYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+    public static void task2() {
+        System.out.println("Задача 2");
+    }
+    public static void defineVersionApp(int mobileOs, int mobileYear) {
+        int currentYear = LocalDate.now().getYear();
+        String mobileOsName = defineOsName(mobileOs);
+
+        if (mobileYear < currentYear) {
+            System.out.println("Для ОС " + mobileOsName + " необходимо установить lite-версию приложения");
+        } else {
+            System.out.println("Для ОС " + mobileOsName + " необходимо установить обычную версию приложения");
+        }
+    }
+    public static String defineOsName(int mobileOs) {
+        String mobileOsName;
+
+        switch (mobileOs) {
+            case 0:
+                mobileOsName = "IOS";
+                break;
+            case 1:
+                mobileOsName = "Android";
+                break;
+            default:
+                mobileOsName = "Неизвестная ОС";
+        }
+        return mobileOsName;
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+    }
+
+    public static int calcNumberDaysDelivery(int distance) {
+
+        if (distance <= 20) {
+            return 1;
+        } else if (distance > 20 && distance <= 60) {
+            return 2;
+        } else if (distance > 60 && distance <= 100) {
+            return 3;
+        } else {
+            return -1;
+        }
+    }
+    public static void printNumberDaysDelivery(int distance) {
+        int days = calcNumberDaysDelivery (distance);
+
+        if (days > 0) {
+            System.out.println("Потребуется дней " + days);
+        } else {
+            System.out.println("Доставка не осуществляется");
         }
     }
 }
